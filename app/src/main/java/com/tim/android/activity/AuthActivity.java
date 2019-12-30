eb1a0aaaaff77ff8f3105be0ed6d99d8ea727f6e
wuj <wuj@hunter.com>
2019-12-30 15:25:07 +0800

    完善界面.

--- app/src/main/java/com/tim/android/activity/AuthActivity.java
+++ app/src/main/java/com/tim/android/activity/AuthActivity.java
@@ -2,10 +2,8 @@ package com.tim.android.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.tim.common.AuthorizedEvent;
-import com.tim.common.DeviceUtils;
 import com.tim.common.Logger;
 import com.tim.iot.R;
 import org.greenrobot.eventbus.EventBus;
@@ -21,14 +19,11 @@ import org.greenrobot.eventbus.ThreadMode;
 public class AuthActivity extends AppCompatActivity {
     private static final String TAG = "AuthActivity";
     private static final Logger logger = Logger.getLogger(TAG);
-    TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        tvInfo = findViewById(R.id.tv_info);
-        tvInfo.setText(String.format("Device:%s", DeviceUtils.getDeviceSerial()));
         logger.d("onCreate " + getTaskId());
         EventBus.getDefault().register(this);
     }
@@ -61,5 +56,10 @@ public class AuthActivity extends AppCompatActivity {
     @Subscribe(threadMode = ThreadMode.ASYNC)
     public void when(AuthorizedEvent event) {
         finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //屏蔽返回键
     }
 }
